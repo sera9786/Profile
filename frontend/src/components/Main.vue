4742e443e284939bb80e15a5aa02909a16355657
Imhunwoo <hunwoo0505@gmail.com>
2020-03-16 16:19:13 +0900

    bind

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,10 +1,21 @@
 <template>
-    <profile/>
+    <profile v-bind="name"/>
 </template>
 
 <script> 
 import Profile from "./Profile"
 export default {
+  data(){
+    return{
+            name: "임헌우",
+            faceBook:"https://www.facebook.com/profile.php?id=100010910586326",
+            email:"hunwoo0505@gmail.com",
+            call:"010-7501-1458",
+            major:"정보보안",
+            field:"frontend",
+            persona:"시바견"
+    }
+  },
   components:{
     profile: Profile,
   }
--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -22,11 +22,11 @@
         </div>
 
         <div class="text">
-          <div>{{major}}</div>
+          <div>{{this.a}}</div>
+          <!-- <div class="border">|</div>
+          <div>{{data}}</div>
           <div class="border">|</div>
-          <div>{{field}}</div>
-          <div class="border">|</div>
-          <div>{{persona}}</div>
+          <div>{{data}}</div> -->
         </div>
       </div>
       
@@ -36,15 +36,10 @@
 
 <script>
 export default {
+    props:[name],
     data() { 
         return {
-            name: "임헌우",
-            faceBook:"https://www.facebook.com/profile.php?id=100010910586326",
-            email:"hunwoo0505@gmail.com",
-            call:"010-7501-1458",
-            major:"정보보안",
-            field:"frontend",
-            persona:"시바견"
+            a:this.name
         }
     },
     methods:{
