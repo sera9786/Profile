9de2fcb383ae783f4123c6f8ff8369f44950054d
Imhunwoo <hunwoo0505@gmail.com>
2020-03-20 08:46:08 +0900

    binding

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,5 +1,5 @@
 <template>
-    <profile v-bind="name"/>
+    <profile v-bind="jsondata"/>
 </template>
 
 <script> 
@@ -7,13 +7,13 @@ import Profile from "./Profile"
 export default {
   data(){
     return{
-            name: "임헌우",
+      jsondata : { name: "임헌우",
             faceBook:"https://www.facebook.com/profile.php?id=100010910586326",
             email:"hunwoo0505@gmail.com",
             call:"010-7501-1458",
             major:"정보보안",
             field:"frontend",
-            persona:"시바견"
+            persona:"시바견"}
     }
   },
   components:{
--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -10,23 +10,23 @@
 
       <div class=" bottom-section">
         <div class="social-media">
-          <a v-bind:href="faceBook">
+          <a href="https://www.facebook.com/profile.php?id=100010910586326" target="_blank">
             <img src="../assets/facebook.png">
           </a>
-           <a onclick="alert('저의 email은 hunwoo0505@gmail.com 입니다')">
+           <a onclick="emailInfo()">
             <img src="../assets/email.png">
           </a>
-           <a onclick="alert('저의 전화번호는 010-7501-1458 입니다.')">
+           <a onclick="callInfo()">
             <img src="../assets/phone.png"> 
           </a>
         </div>
 
         <div class="text">
-          <div>{{this.a}}</div>
-          <!-- <div class="border">|</div>
-          <div>{{data}}</div>
+          <div>{{this.showMajor}}</div>
           <div class="border">|</div>
-          <div>{{data}}</div> -->
+          <div>{{this.showField}}</div>
+          <div class="border">|</div>
+          <div>{{this.showPersona}}</div>
         </div>
       </div>
       
@@ -36,18 +36,24 @@
 
 <script>
 export default {
-    props:[name],
+    props:['name', 'facebook', 'email', 'call', 'major', 'field', 'persona'],
     data() { 
         return {
-            a:this.name
+            showName:this.name,
+            showFacebook:this.facebook,
+            showEmail:this.email,
+            showCall:this.call,
+            showMajor:this.major,
+            showField:this.field,
+            showPersona:this.persona,
         }
     },
     methods:{
       emailInfo: ()=>{
-          alert("저의 email은" + this.email + "입니다.")
+          alert("저의 email은" + this.showEmail + "입니다.")
       },
       callInfo: ()=>{
-        alert("저의 전화번호는" + this.call + "입니다");
+        alert("저의 전화번호는" + this.showCall + "입니다");
       }
     }
 }
